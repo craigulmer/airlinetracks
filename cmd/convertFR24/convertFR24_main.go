9d1faea644bfe6264f7741735c983fc80a2ceac4
Craig Ulmer <cdulmer@sandia.gov>
2016-01-02 22:01:08 -0800

    FIX: Found a few problems w/ relabels. Changed, but still lacking FMT: Ran fmt on code, triggering changes

--- cmd/convertFR24/convertFR24_main.go
+++ cmd/convertFR24/convertFR24_main.go
@@ -20,141 +20,165 @@ package main
 // 16 : Airlinecode (UAL1431)
 // 17 : Estimated arrival time
 
-
 import (
-	"fmt"
-	"strings"
-	"sort"
-	"os"
-	"flag"
 	"encoding/json"
-	"io/ioutil"
+	"flag"
+	"fmt"
 	atx "github.com/craigulmer/airlinetracks"
-	
+	"io/ioutil"
+	"os"
+	"sort"
+	"strings"
 )
 
-func parseFile(src_file string, mm map[string]Markers, fin2aid map[string]string) (int,int,int) {
+func parseFile(src_file string,
+	mm map[string]Markers, fin2aid map[string]string) (int, int, int, int) {
 
-	discarded:=0
-	imported:=0
-	repaired:=0
+	discarded := 0
+	imported := 0
+	repaired := 0
+	duplicates := 0
 
-	fi, err := os.Open(src_file);
-	if err != nil { panic(err) }
-	defer func(){
+	fi, err := os.Open(src_file)
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
 		if err := fi.Close(); err != nil {
 			panic(err)
 		}
 	}()
-		
-	dec := json.NewDecoder(fi);
+
+	dec := json.NewDecoder(fi)
 	for {
-		var entries map[string]interface{} 
-		if err:= dec.Decode(&entries); err != nil { break } // panic(err) }
-			
+		var entries map[string]interface{}
+		if err := dec.Decode(&entries); err != nil {
+			break
+		} // panic(err) }
+
 		for ref := range entries {
 			//Ref is json id
 			if ref == "full_count" { continue }
 			if ref == "version"    { continue }
 
 			//fmt.Println(f.Name()+" "+ref);
-			vals:=entries[ref].([]interface{})
+			vals := entries[ref].([]interface{})
 
-			aid := vals[0].(string); //aid
-			fin := vals[9].(string); //fin
+			aid := vals[0].(string) //aid
+			fin := vals[9].(string) //fin
 
+			if aid == "" {
 
-			if aid=="" {
-				prv_aid,found := fin2aid[fin]
+				if fin=="" {
+					discarded++
+					continue
+				}
+
+				prv_aid, found := fin2aid[fin]
 				if !found {
 					discarded++
 					continue
 				}
-				aid=prv_aid
+				aid = prv_aid
 				repaired++
 			} else {
 				imported++
 			}
-				
+
 			new_marker := marker{
-				ts:  int64(vals[10].(float64)), 
+				ts:  int64(vals[10].(float64)),
 				lat: vals[1].(float64),
 				lon: vals[2].(float64),
 				alt: vals[4].(float64),
 				src: vals[11].(string),
 				dst: vals[12].(string),
-				fid: vals[13].(string),  //airline code
-				fin: fin }               //registration
+				fid: vals[13].(string), //airline code
+				fin: fin}               //registration
 
 			fin2aid[fin] = aid
-			marker_array := mm[ aid ]; //ref
-			mm[ aid ] = append(marker_array, &new_marker)
+			marker_array := mm[aid] //ref
+			num_pts:=len(marker_array)
+			if num_pts>0 {
+				if marker_array[num_pts-1].ts == new_marker.ts {
+					if !new_marker.IsIdentical(marker_array[num_pts-1]) {
+						fmt.Println("Duplicates, but not identical: (previous first)",aid,"-->",fin,"\n",
+							marker_array[num_pts-1].ToString(),"\n",
+							new_marker.ToString())
+					} else {
+						fmt.Println("Duplicate IS identical")
+					}
+
+					duplicates++
+					continue
+				}
+			}
+			mm[aid] = append(marker_array, &new_marker)
 		}
 	}
-	return discarded,imported,repaired
+	return discarded, imported, repaired, duplicates
 }
 
-func parseDayDirectory(src_dir string, dst_file_name string){
+func parseDayDirectory(src_dir string, dst_file_name string) {
 
-	discarded:=0
-	imported:=0
-	repaired:=0
-	mm:=make(map[string]Markers)
-	fin2aid:=make(map[string]string)
-	
-	files,_ := ioutil.ReadDir(src_dir)
-	for _,f := range files {
-		bad,ok,fixed := parseFile(src_dir+"/"+f.Name(), mm, fin2aid)
-		discarded+=bad
-		imported+=ok
-		repaired+=fixed
-		fmt.Println("mm size: ",len(mm), " Total Ok: ",imported,"Total Discarded: ", discarded,"Total Repaired: ", repaired)
+	discarded := 0
+	imported := 0
+	repaired := 0
+	duplicates := 0
+	mm := make(map[string]Markers)
+	fin2aid := make(map[string]string)
+
+	files, _ := ioutil.ReadDir(src_dir)
+	for _, f := range files {
+		tmp_discarded, tmp_imported, tmp_repaired, tmp_duplicates := parseFile(src_dir+"/"+f.Name(), mm, fin2aid)
+		discarded += tmp_discarded
+		imported += tmp_imported
+		repaired += tmp_repaired
+		duplicates += tmp_duplicates
+		fmt.Println("mm size: ", len(mm), " Total Ok: ", imported, 
+			"Total Discarded: ", discarded, "Total Repaired: ", repaired,
+			"Total Duplictes: ", duplicates)
 	}
 
-	
 	//Sort every entry's vals by time
 	tracks := make([]*atx.Track, len(mm))
-	spot:=0
+	spot := 0
 	for aid, m := range mm {
 		//fmt.Println("#\t",id,"\tFLIGHT\t",k,"\t",srcdst[k] )
 		sort.Sort(byTime{m})
-		tracks[spot]=m.ToATXTrack(aid)
+		tracks[spot] = m.ToATXTrack(aid)
 		fmt.Println(tracks[spot].ToString())
 		spot++
 	}
 
-	fmt.Println("Total Ok:", imported,"Total Discarded:",discarded)
-	
+	fmt.Println("Total Ok:", imported, "Total Discarded:", discarded, "Total Duplicates:",duplicates)
+
 	sort.Sort(atx.ByAID{tracks})
 
-
-	tw,err:=atx.OpenWriter(dst_file_name)
-	if err!=nil {
-		fmt.Println("Error opening output file",dst_file_name,err)
+	tw, err := atx.OpenWriter(dst_file_name)
+	if err != nil {
+		fmt.Println("Error opening output file", dst_file_name, err)
 		return
 	}
-	for _,t := range tracks {
+	for _, t := range tracks {
 		//fmt.Println(v.ToString())
 		tw.Append(t)
 	}
 	tw.Close()
-	
-}
 
+}
 
 func makeOutputFilename(idir string, odir string) string {
 	args := strings.Split(idir, "/")
-	return odir+"/"+args[len(args)-1]+".gz"
+	return odir + "/" + args[len(args)-1] + ".gz"
 }
 
-func main(){
+func main() {
 
-	
 	var idir = flag.String("input", "data/2014-03-31", "Input Directory")
 	var odir = flag.String("output", "./", "Output Directory")
 	flag.Parse()
-	fmt.Println("Input Directory: "+*idir+" Output Directory: "+*odir)
-	
-	fmt.Println("Val is "+makeOutputFilename(*idir,*odir))
+	fmt.Println("Input Directory: " + *idir + " Output Directory: " + *odir)
+
+	fmt.Println("Val is " + makeOutputFilename(*idir, *odir))
 	parseDayDirectory(*idir, makeOutputFilename(*idir, *odir))
 }
--- cmd/convertFR24/marker.go
+++ cmd/convertFR24/marker.go
@@ -1,89 +1,93 @@
 package main
 
 import (
-	"fmt"
 	"bytes"
+	"fmt"
 	atx "github.com/craigulmer/airlinetracks"
-	
 )
 
 type marker struct {
-	ts  int64    //10
-	lat float64  //1
-	lon float64  //2
-   alt float64  //4
-	src string   //11
-	dst string   //12
-	fid string   //13 airline code
-	fin string   //9  registratioin
+	ts  int64   //10
+	lat float64 //1
+	lon float64 //2
+	alt float64 //4
+	src string  //11
+	dst string  //12
+	fid string  //13 airline code
+	fin string  //9  registratioin
 }
 type Markers []*marker
 
-func (m Markers) Len() int { return len(m) }
-func (m Markers) Swap(i,j int) { m[i],m[j] = m[j],m[i] }
+func (m Markers) Len() int      { return len(m) }
+func (m Markers) Swap(i, j int) { m[i], m[j] = m[j], m[i] }
 
 type byTime struct{ Markers }
-func (m byTime) Less(i,j int) bool { return m.Markers[i].ts < m.Markers[j].ts }
+
+func (m byTime) Less(i, j int) bool { return m.Markers[i].ts < m.Markers[j].ts }
 
 func (m1 marker) ToString() string {
 	return fmt.Sprintf("%d\t%f\t%f\t%f\t%s\t%s", m1.ts, m1.lat, m1.lon, m1.alt, m1.fin, m1.fid)
 }
-
-
+func (m1 *marker) IsIdentical(m2 *marker) bool {
+	return m1.ts == m2.ts &&
+		m1.lat == m2.lat &&
+		m1.lon == m2.lon &&
+		m1.alt == m2.alt &&
+		m1.fin == m2.fin &&
+		m1.MetaIsSame(m2)
+}
 func (m1 *marker) MetaIsSame(m2 *marker) bool {
-	return m1.src==m2.src &&
-		m1.dst==m2.dst &&
-		m1.fid==m2.fid	
+	return m1.src == m2.src &&
+		m1.dst == m2.dst &&
+		m1.fid == m2.fid
 }
 
 func (m Markers) ToATXMeta() []atx.Meta {
 	//fmt.Println("marker num points: ",len(m))
-	meta := make([]atx.Meta,0,len(m))
-	for i,o := range m {
-		if i>0 && m[i-1].MetaIsSame(o) { continue }
-		mnew:= atx.Meta{First_point_id:i, Flt:o.fid, Src:o.src, Dst:o.dst}
-		meta = append(meta,mnew)
+	meta := make([]atx.Meta, 0, len(m))
+	for i, o := range m {
+		if i > 0 && m[i-1].MetaIsSame(o) {
+			continue
+		}
+		mnew := atx.Meta{First_point_id: i, Flt: o.fid, Src: o.src, Dst: o.dst}
+		meta = append(meta, mnew)
 	}
 	return meta
 }
 func (m Markers) ToATXPoints() []atx.Point {
 	points := make([]atx.Point, len(m), len(m))
-	for i,o := range m {
+	for i, o := range m {
 		points[i] = atx.Point{X: o.lon, Y: o.lat, Z: o.alt, T: o.ts}
 	}
 	return points
 }
 func (m Markers) ToATXTrack(aid string) *atx.Track {
-	t := atx.Track{Aid:aid, Fin: m[0].fin, Fid: m[0].fid, 
+	t := atx.Track{Aid: aid, Fin: m[0].fin, Fid: m[0].fid,
 		Points: m.ToATXPoints(),
-		Meta: m.ToATXMeta() }
+		Meta:   m.ToATXMeta()}
 	return &t
 }
 
-
 func (m Markers) ToString(id int) {
-	for _,o := range m {
-		fmt.Printf("%d\t%d\t%f\t%f\t%d\n", id, o.ts, o.lat, o.lon, o.alt);
+	for _, o := range m {
+		fmt.Printf("%d\t%d\t%f\t%f\t%d\n", id, o.ts, o.lat, o.lon, o.alt)
 	}
 }
 func (m Markers) ToWKT() string {
 	var buffer bytes.Buffer
-	buffer.WriteString("LINESTRING (");
-	is_first := true;
-	for _,o := range m {
-		if(!is_first){
-			buffer.WriteString(", ");
+	buffer.WriteString("LINESTRING (")
+	is_first := true
+	for _, o := range m {
+		if !is_first {
+			buffer.WriteString(", ")
 		} else {
-			is_first = false;
+			is_first = false
 		}
-		s := fmt.Sprintf("%f %f %d %d", o.lon, o.lat, o.alt, o.ts);
-		buffer.WriteString(s);
+		s := fmt.Sprintf("%f %f %d %d", o.lon, o.lat, o.alt, o.ts)
+		buffer.WriteString(s)
 		//buffer.WriteString(o.lon+" "+o.lat+" "+o.alt+" ",o.ts);
 		//fmt.Printf("%d\t%d\t%f\t%f\t%d\n", id, o.ts, o.lat, o.lon, o.alt);
 	}
-	buffer.WriteString(")");
-	return buffer.String();
+	buffer.WriteString(")")
+	return buffer.String()
 }
-
-
-
